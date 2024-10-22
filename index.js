05550669deca041dfdc4b658d951f51b2e67b665
Axel Liscovsky <axelliscovsky@outlook.com.ar>
2024-10-22 13:01:11 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    jQuery("#btn_bicarbonato").on("click",function(){
+        window.open('index_bicarbonato.htm', '_self');
+    });
+    jQuery("#btn_percarbonato").on("click",function(){
+        window.open('index_percarbonato.htm', '_self');
+    });
+    jQuery("#btn_acido_citrico").on("click",function(){
+        window.open('index_acido_citrico.htm', '_self');
+    });
+    jQuery("#btn_diatomeas").on("click",function(){
+        window.open('index_diatomeas.htm', '_self');
+    });
+    jQuery("#btn_magic").on("click",function(){
+        window.open('index_magicpowder.htm', '_self');
+    });
+    jQuery("#btn_magicc").on("click",function(){
+        window.open('index_magicpowderc.htm', '_self');
+    });
+    jQuery("#btn_vinagre").on("click",function(){
+        window.open('index_vinagre.htm', '_self');
+    });
+    jQuery("#btn_borax").on("click",function(){
+        window.open('index_borax.htm', '_self');
+    });
+    jQuery("#btn_carbonato").on("click",function(){
+        window.open('index_carbonato.htm', '_self');
+    });
+});
